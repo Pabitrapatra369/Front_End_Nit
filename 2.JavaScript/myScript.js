b9650baecd2ca8e8d2cc8f310cb8d16ebd86c899
Pabitra Patra <pabitrapatra369@gmail.com>
2024-07-29 23:39:39 +0530

    first commit

---
+++ 2.JavaScript/myScript.js
@@ -0,0 +1,11 @@
+/* Example on defining external functinos  */
+
+function fun1(){
+	document.write("hi,i am fun1");
+}
+
+function fun2(){
+	document.write("hi, i am fun2");
+}
+/* Note:
+save this file with a name "myscript.js" in current location*/
