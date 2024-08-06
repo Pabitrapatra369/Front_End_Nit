c45a13d9e65718dfc7e7daf8cb3ec301bcf8dba4
Pabitra Patra <pabitrapatra369@gmail.com>
2024-08-06 23:31:34 +0530

    some converter projects

--- 2.JavaScript/interest.js
+++ 2.JavaScript/interest.js
@@ -1,1 +1,16 @@
-s
+//defining a function to find the interest amount
+
+function calcAmount(){
+	let p,r,t,ia,ta;
+	//data collection
+	p=pamt.value;
+	r=roi.value;
+	t=time.value;
+	//calc
+	ia=p*r*t/100;
+	ta=+p + ia;
+	
+	//displaing outputs
+	intAmt.innerHTML="Intrest amount  &#8377; &nbsp;"+ia;
+	totAmt.innerHTML="Total amount &#8377; &nbsp;"+ta;
+}
