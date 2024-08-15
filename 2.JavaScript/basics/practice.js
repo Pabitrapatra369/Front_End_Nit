d754c22f7d71275e98c239053ab84c73620bdb2a
Pabitra Patra <pabitrapatra369@gmail.com>
2024-08-15 21:07:15 +0530

    practice

--- 2.JavaScript/basics/practice.js
+++ 2.JavaScript/basics/practice.js
@@ -1,15 +1,8 @@
-let n = 145;
-let m = n;
-let d;
-let s = 0;
-let f;
-while (m) {
-  d = m % 10;
-  f = 1;
-  if (d != 0) for (let j = d; j >= 1; j--) f = f * j;
-  s = s + f;
-  m = Math.floor(m / 10);
-}
+const a = [10, 20, 30, 40, 50];
 
-if (s === n) op.value = "strong Number";
-else op.value = "Not a strong Number";
+let b = [...a];
+//console.log(a);
+
+for (let e of a) {
+  console.log(e);
+}
